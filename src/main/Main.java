63809c5ab8ffa853765561f6a675b25e0bae105b
adamb <adam.brents1@gmail.com>
2022-07-31 11:57:53 -0500

    1: init commit

---
+++ src/main/Main.java
@@ -0,0 +1,10 @@
+package main;
+
+import database.JDBC;
+
+public class Main {
+    public static void main(String[] args) {
+        JDBC.openConnection();
+        JDBC.closeConnection();
+    }
+}
